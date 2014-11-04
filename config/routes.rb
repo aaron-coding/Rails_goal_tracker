508920ac0e08298722c251c32b4a0817264ed736
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-03 17:41:44 -0800

    Add Comments Spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show] do
-    resources :goals, only: [:new, :edit, :update]
+    resources :goals, only: [:new, :edit, :update, :show]
+    resources :comments, only: [:create]
   end
-  resources :goals, only: [:create, :destroy]
+  
+  resources :goals, only: [:create, :destroy] do
+    resources :comments, only: [:create]
+  end
+  
   resource :session, only: [:new, :create, :destroy]
   
   root to: "users#home"
